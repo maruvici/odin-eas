2bff7e525149d8983180e8c409462d95edb2adbd
Mav <196895938+maruvici@users.noreply.github.com>
2025-11-24 22:46:29 +0800

    feat: Change squares color on mouse hover

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,9 @@ function createGrid(size = 16) {
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseover", (e) => {
+      square.style.backgroundColor = "black";
+    });
     grid.appendChild(square);
   }
 }
